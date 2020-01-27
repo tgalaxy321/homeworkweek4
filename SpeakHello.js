c50dd49e91df28ac8838900c3fc2d423c13e7552
tgalaxy321 <59703233+tgalaxy321@users.noreply.github.com>
2020-01-27 18:29:13 -0500

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,6 +1,14 @@
 // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
 // See Lecture 52, part 2
 
+(function(window){
+	var helloSpeaker = {};
+	var speakWord = "Hello";	
+	helloSpeaker.speak = function (name) {
+  	console.log(speakWord + " " + name);
+	}
+	window.helloSpeaker = helloSpeaker;
+})(window);
 
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
